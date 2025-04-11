838c4ae14b8f22c3015fe544ec509b666f872b42
Yusuf Günaydın <yusuf.gunaydin@ohayocorp.com>
2025-04-11 19:03:56 +0300

    Add process.cwd and process.chdir.

--- process/module.go
+++ process/module.go
@@ -11,12 +11,36 @@ import (
 const ModuleName = "process"
 
 type Process struct {
-	env map[string]string
+	runtime *sobek.Runtime
+	env     map[string]string
+}
+
+func (p *Process) cwd(call sobek.FunctionCall) sobek.Value {
+	wd, err := os.Getwd()
+	if err != nil {
+		panic(p.runtime.ToValue(err))
+	}
+
+	return p.runtime.ToValue(wd)
+}
+
+func (p *Process) chdir(call sobek.FunctionCall) sobek.Value {
+	if len(call.Arguments) == 0 {
+		panic(p.runtime.ToValue("chdir requires a path argument"))
+	}
+
+	path := call.Arguments[0].String()
+	if err := os.Chdir(path); err != nil {
+		panic(p.runtime.ToValue(err))
+	}
+
+	return sobek.Undefined()
 }
 
 func Require(runtime *sobek.Runtime, module *sobek.Object) {
 	p := &Process{
-		env: make(map[string]string),
+		runtime: runtime,
+		env:     make(map[string]string),
 	}
 
 	for _, e := range os.Environ() {
@@ -26,6 +50,8 @@ func Require(runtime *sobek.Runtime, module *sobek.Object) {
 
 	o := module.Get("exports").(*sobek.Object)
 	o.Set("env", p.env)
+	o.Set("cwd", p.cwd)
+	o.Set("chdir", p.chdir)
 }
 
 func Enable(runtime *sobek.Runtime) {
